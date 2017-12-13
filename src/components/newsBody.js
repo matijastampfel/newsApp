ebba1e081b0ae991576d447c62b7417ef17e7185
matijastampfel <matija.stampfel@gmail.com>
2017-12-13 15:23:37 +0100

    api adress bar change

--- src/components/newsBody.js
+++ src/components/newsBody.js
@@ -3,6 +3,7 @@
 //
 import React, { Component } from "react";
 import axios from "axios";
+import Image from "react-image-resizer";
 //
 // ────────────────────────────────────────────────────────────────── IMPORTS ─────
 //
@@ -12,7 +13,6 @@ import axios from "axios";
 //
 
 let posts = [];
-let newsImage= '';
 
 //
 // ─────────────────────────────────────────────────────────────────── VARS ─────
@@ -31,24 +31,20 @@ export class NewsBody extends Component {
   //
   // ────────────────────────────────────────────────────────────── CONSTRUCTOR ─────
   //
-  
+
   componentDidMount() {
     axios
       .get(
-        "https://newsapi.org/v2/top-headlines?" +
-          "sources=bbc-news&" +
-          "apiKey=914f1a282559480ba84f1513c9eb320d"
+        "https://newsapi.org/v2/everything?q=siria&apiKey=914f1a282559480ba84f1513c9eb320d"
       )
       .then(res => {
         console.log(res);
         posts = res.data.articles;
         this.setState({
           posts
-        })
-      })
+        });
+      });
   }
-
- 
 
   //
   // ───────────────────────────────────────────────────────────────── HANDLERS ─────
@@ -57,13 +53,20 @@ export class NewsBody extends Component {
   render() {
     return (
       <div className="news">
-        <ul>
-          
+        <ul className="newsList">
           {this.state.posts.map((post, idx) => (
+            <div className='newsContainer'>
             <li key={idx}>
-            <img src={post.urlToImage} alt=""/>
-             {post.title} 
-             </li>
+              <div className='newsImage'>
+                <Image src={post.urlToImage} alt="" width={340} height={140} />
+              </div>
+              <div className='newsTitle'>
+                <h1>{post.title}</h1>
+                {post.description}
+              </div>
+            </li>
+            <br/>
+            </div>
           ))}
         </ul>
       </div>
